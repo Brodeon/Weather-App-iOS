d62a5bb2897dbc892e5e9bfefcd84d412e128ba3
Przemek <przemek@LinuxPluss-Mac.local>
2019-02-14 14:23:02 +0100

    Initial Commit

---
+++ Weather App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Weather App
+//
+//  Created by Przemek on 2/14/19.
+//  Copyright © 2019 Przemek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
